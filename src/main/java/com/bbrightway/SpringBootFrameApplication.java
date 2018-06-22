72ccc465bded3fcdd90745798fea6baae1a747bc
李雪云 <lixueyun@bbrightway.com>
2018-06-22 10:40:42 +0800

    增加spring cloud

--- src/main/java/com/bbrightway/SpringBootFrameApplication.java
+++ src/main/java/com/bbrightway/SpringBootFrameApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -13,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @ComponentScan(basePackages = "com.bbrightway")
 @MapperScan("com.bbrightway.**/repository/**")
 @EnableSwagger2
+@EnableEurekaClient
 public class SpringBootFrameApplication {
 
 	public static void main(String[] args) {
