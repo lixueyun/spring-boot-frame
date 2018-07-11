e55d67bf8a5689c6d56131b55673cf0e626c84b3
“ <l>
2018-07-11 14:30:20 +0800

    修改pom依赖

--- src/main/java/com/bbrightway/SpringBootFrameApplication.java
+++ src/main/java/com/bbrightway/SpringBootFrameApplication.java
@@ -3,11 +3,8 @@ package com.bbrightway;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
