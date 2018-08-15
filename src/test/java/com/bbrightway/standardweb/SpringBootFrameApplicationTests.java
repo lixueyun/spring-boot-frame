683df3517d10aef27a8b98ca90a915980edcd417
lixueyun <lixueyun186@163.com>
2018-08-15 21:43:49 +0800

    test修改

--- src/test/java/com/bbrightway/standardweb/SpringBootFrameApplicationTests.java
+++ src/test/java/com/bbrightway/standardweb/SpringBootFrameApplicationTests.java
@@ -11,6 +11,12 @@ public class SpringBootFrameApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		System.out.println("test");
+	}
+
+	@Test
+	public void test(){
+		System.out.println("test");
 	}
 
 }
