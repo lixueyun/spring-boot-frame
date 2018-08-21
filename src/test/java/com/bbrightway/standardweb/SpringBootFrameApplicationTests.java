75dee20ae583f836d3b2992a9357437987325dac
lixy16 <lixy16@ziroom.com>
2018-08-21 15:45:59 +0800

    测试

--- src/test/java/com/bbrightway/standardweb/SpringBootFrameApplicationTests.java
+++ src/test/java/com/bbrightway/standardweb/SpringBootFrameApplicationTests.java
@@ -5,6 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootFrameApplicationTests {
@@ -23,4 +28,18 @@ public class SpringBootFrameApplicationTests {
 	}
 
 
+
+	public static void main(String[] args) {
+		Date date = new Date(118,8,1);
+		LocalDateTime localDateTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+		Date date1 = new Date();
+		LocalDateTime localDateTime1 = LocalDateTime.ofInstant(date1.toInstant(), ZoneId.systemDefault());
+		System.out.println(localDateTime.isAfter(localDateTime1));
+		System.out.println(date.after(date1));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println(sdf.format(date));
+		System.out.println(sdf.format(date1));
+		System.out.println("test1");
+	}
 }
